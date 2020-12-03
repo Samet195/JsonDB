4fcb507f8ffa0c2b52a3be2d3841251edb47832c
Samet195 <70340667+Samet195@users.noreply.github.com>
2020-12-03 14:11:15 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,9 +2,9 @@ from JsonDB import JsonDB
 
 db = JsonDB("ExampleDB.json")
 
-print(db.showTables())
+print(db.show_tables())
 
-print(db.getCols("Users"))
+print(db.get_cols("Users"))
 
 print(db.select("Users"))
 
