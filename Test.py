43e9530c8e220f19f7122fc13d1a3de20b822f88
Samet195 <70340667+Samet195@users.noreply.github.com>
2020-12-03 13:47:11 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,18 +2,18 @@ from JsonDB import JsonDB
 
 db = JsonDB("ExampleDB.json")
 
-print(db.ShowTables())
+print(db.showtables())
 
-print(db.GetCols("Users"))
+print(db.getcols("Users"))
 
-print(db.Select("Users"))
+print(db.select("Users"))
 
-print(db.Select("Users",col="ID"))
+print(db.select("Users",col="ID"))
 
-print(db.Select("Users",col="User"))
+print(db.select("Users",col="User"))
 
-print(db.Select("Users",where={"ID":0}))
+print(db.select("Users",where={"ID":0}))
 
-print(db.Select("Users",where={"Pass":"pass"}))
+print(db.select("Users",where={"Pass":"pass"}))
 
 input("\n--Enter_to_Continue--")
