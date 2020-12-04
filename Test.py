95eedcae25378e96ca2f191059ad27e58dc316bd
Samet195 <70340667+Samet195@users.noreply.github.com>
2020-12-04 13:25:54 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,10 +1,10 @@
 from JsonDB import JsonDB
-
 db = JsonDB("ExampleDB.json")
 
 print(db.show_tables())
 
 print(db.get_cols("Users"))
+
 
 print(db.select("Users"))
 
@@ -17,5 +17,6 @@ print(db.select("Users",where={"ID":0}))
 print(db.select("Users",where={"Pass":"pass"}))
 
 print(db.select("Users",col="User" ,where={"Pass":"pass"}))
+
 
 input("\n--Enter_to_Continue--")
