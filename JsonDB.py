99812080444b33f77298952b13af8fc5933996f1
Samet195 <70340667+Samet195@users.noreply.github.com>
2020-12-06 12:05:04 +0300

    Update JsonDB.py

--- JsonDB.py
+++ JsonDB.py
@@ -3,11 +3,16 @@ from os import path
 
 class JsonDB:
   def __init__(self,db):
-    if path.exists(db) and path.isfile(db):
+    if not db:
+      raise Exception("Path is empty")
+
+    elif path.exists(db) and path.isfile(db):
       self.DB = loads(open(db,"r+",encoding="utf-8").read())
 
     elif path.isdir(db):
       raise Exception("Path is a directory")
+
+    else:raise Exception("Path is not found")
 
   def show_tables(self):
     return self.DB["TableList"]
